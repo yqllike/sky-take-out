d85f9aa94f119bcc1527e107a65bff3b7e35122b
Qiliang Yang <yangqiliang31@gmail.com>
2024-10-08 17:35:55 -0400

    新增套餐管理代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -2,10 +2,12 @@ package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 //import com.sun.org.apache.xalan.internal.xsltc.compiler.util.ResultTreeType;
+import com.sky.vo.DishVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -61,4 +63,37 @@ public class DishController {
         return Result.success();
     }
 
+    @GetMapping("/{id}")
+    @ApiOperation("跟据id查询菜品")
+    public Result<DishVO> getById(@PathVariable Long id){
+        log.info("跟据id查询菜品:{}",id);
+        DishVO dishVO = dishService.getByIdwithFlavor(id);
+        return Result.success(dishVO);
+    }
+
+    /**
+     * 修改菜品
+     * @param dishDTO
+     * @return
+     */
+    @PutMapping
+    @ApiOperation("修改菜品")
+    public Result update(@RequestBody DishDTO dishDTO){
+        log.info("修改菜品:{}",dishDTO);
+        dishService.updateWithFlavor(dishDTO);
+        return Result.success();
+
+    }
+
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    @GetMapping("/list")
+    @ApiOperation("根据分类id查询菜品")
+    public Result<List<Dish>> list(Long categoryId){
+        List<Dish> list = dishService.list(categoryId);
+        return Result.success(list);
+    }
 }
